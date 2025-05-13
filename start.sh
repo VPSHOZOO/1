e8613ac68629b7e394a27497c5a2d9043c3f7c22
VPSHOZOO <lordhozoo891@gmail.com>
2025-05-13 21:16:47 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start PulseAudio server
+pulseaudio -D --exit-idle-time=-1
+
+# Create virtual sound devices
+pacmd load-module module-virtual-sink sink_name=v1
+pacmd set-default-sink v1
+pacmd set-default-source v1.monitor
+
+# Start Xfce desktop
+startxfce4 &
+
+# Start VNC server
+x11vnc -display :0 -nopw -forever -xkb -bg -rfbport 5900 -localhost -o /tmp/x11vnc.log &
+
+# Start NoVNC
+websockify --web /usr/share/novnc/ 8080 localhost:5900
